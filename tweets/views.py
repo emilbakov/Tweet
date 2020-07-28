6c231fb9898b713e437af4d3af01fad0383693f2
unknown <eb.bakov@gmail.com>
2020-07-28 15:45:48 +0300

    url api export

--- tweets/views.py
+++ tweets/views.py
@@ -13,7 +13,7 @@ from rest_framework.authentication import SessionAuthentication #default
 from .forms import TweetForm
 
 from .models import Tweet
-from .serializers import TweetSerializer,TweetActionSerializer
+from .serializers import TweetSerializer,TweetActionSerializer,TweetCreateSerializer
 
 ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 
@@ -29,7 +29,7 @@ def home_view(request, *args,**kwargs):
 @authentication_classes([SessionAuthentication])
 @permission_classes([IsAuthenticated]) 
 def tweet_create_view(request,*args,**kwargs):
-    serializer = TweetSerializer(data=request.POST)
+    serializer = TweetCreateSerializer(data=request.POST)
     if serializer.is_valid():
         obj=serializer.save(user=request.user)
         return Response(serializer.data, status=201)
@@ -78,6 +78,7 @@ def tweet_action_view(request,*args,**kwargs):
     serializer = TweetActionSerializer(data=request.data)
     if serializer.is_valid(raise_exception=True):
         data= serializer.validated_data
+        content=data.get("content")
         tweet_id=data.get("id")
         action=data.get("action")
         qs=Tweet.objects.filter(id=tweet_id)
@@ -91,7 +92,10 @@ def tweet_action_view(request,*args,**kwargs):
         elif action == "unlike":
             obj.likes.remove(request.user)
         elif action == "retweet":
-            pass        
+            
+            new_tweet=Tweet.objects.create(user=request.user,parent=obj,content=content)
+            serializer = TweetSerializer(new_tweet)
+            return Response(serializer.data,status=200)      
     return Response({},status=200)
 
 
